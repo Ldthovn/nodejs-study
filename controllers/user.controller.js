eea3fd55a8add7c504785ec8cda96d88c8d40615
Le Dinh Tho <tho.ld@consultindochina.com>
2020-11-16 22:26:59 +0700

    add signed cookie

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -18,7 +18,9 @@ module.exports.search = (req, res) => {
   res.render("users/index", { users: matchedUsers, question: q });
 };
 
-module.exports.create = (req, res) => res.render("users/create");
+module.exports.create = (req, res) => {
+  res.render("users/create");
+};
 
 module.exports.get = (req, res) => {
   var id = req.params.id;
@@ -30,23 +32,6 @@ module.exports.get = (req, res) => {
 
 module.exports.postCreate = (req, res) => {
   req.body.id = shortid.generate();
-  var errors = [];
-
-  if (!req.body.name) {
-    errors.push("Name is required");
-  }
-
-  if (!req.body.phone) {
-    errors.push("Phone is required");
-  }
-
-  if (errors.length) {
-    res.render("users/create", {
-      errors: errors,
-      values: req.body
-    });
-    return;
-  }
 
   db.get("users").push(req.body).write();
   res.redirect("/users");
--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 var express = require("express");
 var bodyParser = require("body-parser");
+var cookieParser = require("cookie-parser");
 
 var userRoute = require("./routes/user.route");
+var authRoute = require("./routes/auth.route");
+
+var authMiddleware = require("./middlewares/auth.middleware");
 
 var port = 3000;
 
@@ -11,11 +15,13 @@ app.set("views", "./views");
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cookieParser('tho080398'));
 
 app.use(express.static("public"));
 
 app.get("/", (req, res) => res.render("index", { name: "AAA" }));
 
-app.use("/users", userRoute);
+app.use("/users", authMiddleware.requireAuth, userRoute);
+app.use("/auth", authRoute);
 
 app.listen(port, () => console.log("Server listening on port " + port));
